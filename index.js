5f660f778b60634e580c9478b44533d9db167436
Slave Atanasov <slaveatanasovmusic@gmail.com>
2019-01-30 15:21:32 +0100

    heroky dplymnt

--- index.js
+++ index.js
@@ -88,11 +88,11 @@ app.use((err, req, res, next) => {
 //Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //Set static folder
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'))
-    })
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const port = process.env.PORT || 5000;
