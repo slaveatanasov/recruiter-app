aff99e7e7cb27aae20ea986d022b3ef5f06f7ce5
Slave Atanasov <slaveatanasovmusic@gmail.com>
2019-01-30 16:50:47 +0100

    changes

--- index.js
+++ index.js
@@ -90,6 +90,17 @@ if (process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('client/build'));
 
+    app.use(jwt({
+        secret: tokenSecret
+    }).unless({
+        path: [
+            {url: '/', methods: ['GET']},
+            {url: '/auth/login', methods: ['POST']},
+            {url: '/users', methods: ['POST']}
+        ]
+    })
+    );
+    
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
