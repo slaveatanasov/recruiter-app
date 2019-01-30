e8c8ffc46ae1163218444e13963bedeeecbba175
Slave Atanasov <slaveatanasovmusic@gmail.com>
2019-01-30 14:28:31 +0100

    prepared for deployment on heroku

--- index.js
+++ index.js
@@ -88,10 +88,10 @@ app.use((err, req, res, next) => {
 //Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
-        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
     })
 }
 
