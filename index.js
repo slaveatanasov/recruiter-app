b9a06fa3db8f834eb010e4847e2382ce3ad4f8eb
Slave Atanasov <slaveatanasovmusic@gmail.com>
2019-01-30 13:40:25 +0100

    preparing for heroku deployment

--- index.js
+++ index.js
@@ -98,4 +98,6 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-app.listen(80);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
